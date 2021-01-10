3c54cac16a2f43d03a19e985062fc112a2a59336
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-01-10 17:12:00 +0000

    Update three.js CDN

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
 const canvas = document.querySelector('canvas');
 
