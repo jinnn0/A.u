294a6f0aabbb0bfe96230659965463f7d0d69a27
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-01-16 22:37:30 +0000

    Fix conflicts in app.js

--- app.js
+++ app.js
@@ -1,69 +1,5 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls';
-<<<<<<< HEAD
-import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
-
-// prettier-ignore
-const colors = ['#4080ff','#ffe940','#40ffff','#fed1ff','#ee00f2','#f25100','#d2d1ff', '#ffa929','#cfff4a','#fff200','#8efabf','#0026ff','#ff4f6f'];
-
-let renderer, scene, camera, controls, model;
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-init();
-animate();
-load3dModel();
-
-function init() {
-  // Renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(width, height);
-  renderer.setClearColor('white');
-  renderer.setPixelRatio(window.devicePixelRatio);
-  document.body.appendChild(renderer.domElement);
-
-  // Camera
-  camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
-  camera.position.set(0, 10, 200);
-
-  // Scene
-  scene = new THREE.Scene();
-
-  // Controls
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 50;
-  controls.maxDistance = 250;
-
-  // Lights
-  const light1 = new THREE.AmbientLight(0x404040);
-  const light2 = new THREE.DirectionalLight(0xffffff, 1);
-
-  light2.position.set(1, 3, 2);
-
-  scene.add(light1);
-  scene.add(light2);
-}
-
-function load3dModel() {
-  const loader = new GLTFLoader();
-  loader.load('model/scene.gltf', (gltf) => {
-    model = gltf.scene;
-    scene.add(model);
-
-    model.scale.set(0.1, 0.1, 0.1);
-    model.position.y = -35;
-    model.rotation.x = 0.2;
-    model.rotation.y = 0.5;
-  });
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  if (model) {
-    model.rotation.y += 0.01;
-  }
-=======
 
 // prettier-ignore
 const colors = ['#4080ff','#ffe940','#40ffff', '#374cae', '#fed1ff','#ee00f2','#f25100','#374cae','#d2d1ff', '#ffa929','#cfff4a','#fff200','#8efabf','#0026ff','#ff4f6f'];
@@ -236,9 +172,7 @@ function addPin(lat, lon) {
 
 function animate() {
   requestAnimationFrame(animate);
->>>>>>> project4
 
-  controls.update();
   renderer.render(scene, camera);
 }
 
